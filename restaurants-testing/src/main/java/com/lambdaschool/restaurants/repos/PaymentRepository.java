17764c2749f5b27ba454536b205d3ea8acabf0c7
jrmmba8314 <jrmmba@outlook.com>
2019-11-13 07:32:02 -0800

    Fixing Auditing Issues

--- restaurants-testing/src/main/java/com/lambdaschool/restaurants/repos/PaymentRepository.java
+++ restaurants-testing/src/main/java/com/lambdaschool/restaurants/repos/PaymentRepository.java
@@ -15,6 +15,6 @@ public interface PaymentRepository extends CrudRepository<Payment, Long>
 
     @Transactional
     @Modifying
-    @Query(value = "INSERT INTO RestaurantPayments(restaurantid, paymentid) values (:restaurantid, :paymentid)", nativeQuery = true)
-    void insertIntoRestaurantPayments(long restaurantid, long paymentid);
+    @Query(value = "INSERT INTO RestaurantPayments(restaurantid, paymentid, , created_by, created_date, last_modified_by, last_modified_date) values (:restaurantid, :paymentid, :uname, CURRENT_TIMESTAMP, :uname, CURRENT_TIMESTAMP)", nativeQuery = true)
+    void insertIntoRestaurantPayments(long restaurantid, long paymentid, String uname);
 }
